0e8f261f87ea56d87e32305f98c9091135968fa3
Abid Suhail <abid1294005@gmail.com>
2021-02-21 13:46:08 +0530

    drawer menu clicked,action bar menu click

--- lib/screens/HomeScreen.dart
+++ lib/screens/HomeScreen.dart
@@ -22,8 +22,7 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   NetworkHelper networkHelper = new NetworkHelper();
   List<Post> postsList = new List<Post>();
-  void getAllPosts() async
-  {
+  void getAllPosts() async {
     try {
       Response response = await networkHelper.getAllPosts();
       List<dynamic> decodedJson = jsonDecode(response.body);
@@ -31,9 +30,7 @@ class _HomeScreenState extends State<HomeScreen> {
       setState(() {
         this.postsList = postsList.posts;
       });
-    }
-    catch(e)
-    {
+    } catch (e) {
       print('error : $e');
     }
   }
@@ -43,8 +40,16 @@ class _HomeScreenState extends State<HomeScreen> {
     // TODO: implement initState
     getAllPosts();
     super.initState();
+  }
+
+  void logout() async {
+    Navigator.pushReplacement(
+        context, MaterialPageRoute(builder: (context) => LoginScreen()));
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.setBool('logged_in', false);
 
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -56,75 +61,91 @@ class _HomeScreenState extends State<HomeScreen> {
               accountEmail: Text("Android Developer"),
               currentAccountPicture: CircleAvatar(
                 backgroundColor:
-                Theme.of(context).platform == TargetPlatform.iOS
-                    ? Colors.blue
-                    : Colors.white,
+                    Theme.of(context).platform == TargetPlatform.iOS
+                        ? Colors.blue
+                        : Colors.white,
                 child: Text(
                   "A",
                   style: TextStyle(fontSize: 40.0),
                 ),
               ),
-            ),ListTile(
-            title: Text("Home"),
-           /* trailing: Icon(Icons.arrow_forward),*/
-          ),
-            ListTile(
-              title: Text("Profile"),
-              /*trailing: Icon(Icons.arrow_forward),*/
-            )],
+            ),
+            GestureDetector(
+              onTap: () {
+                Toast.show('HOME', context,
+                    duration: Toast.LENGTH_LONG, gravity: Toast.BOTTOM);
+                Navigator.pop(context);
+              },
+              child: ListTile(
+                title: Text("Home"),
+              ),
+            ),
+            GestureDetector(
+              onTap: () {
+                Toast.show('Profile', context,
+                    duration: Toast.LENGTH_LONG, gravity: Toast.BOTTOM);
+                Navigator.pop(context);
+              },
+              child: ListTile(
+                title: Text("Profile"),
+              ),
+            )
+          ],
         ),
       ),
       appBar: AppBar(
         title: Text('Home'),
+        actions: [
+          PopupMenuButton<String>(
+            onSelected: (choice) {
+              if (choice == 'Logout') {
+                logout();
+              }
+            },
+            itemBuilder: (BuildContext context) {
+              return {'Logout', 'Settings'}.map((String choice) {
+                return PopupMenuItem<String>(
+                  value: choice,
+                  child: Text(choice),
+                );
+              }).toList();
+            },
+          )
+        ],
       ),
       body: Container(
         child: Center(
-          child: Column(
-              children: [
-            Text('My Auth Token is : ${widget.user.getToken()}',
-                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
-            SizedBox(
-              height: 15,
+          child: Column(children: [
+            Container(
+              padding: EdgeInsets.all(8),
+              child: Text('My Auth Token is : ${widget.user.getToken()}',
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
             ),
-            Opacity(
-              opacity: 1,
-              child: FlatButton(
-                color: Colors.red,
-                child: Text('Logout'),
-                onPressed: () async {
-                  SharedPreferences prefs =
-                      await SharedPreferences.getInstance();
-                  prefs.setBool('logged_in', false);
-                  Navigator.pushReplacement(context,
-                      MaterialPageRoute(builder: (context) => LoginScreen()));
-                },
-              ),
+
+            Container(
+              padding: EdgeInsets.all(10),
+              alignment: Alignment.topLeft,
+              child: Text('Data coming from server',
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontStyle: FontStyle.italic)),
             ),
-                Container(
-                  padding: EdgeInsets.all(8),
-                  alignment: Alignment.topLeft,
-                  child: Text('Dummy List data coming from server',
-                      style: TextStyle(fontWeight: FontWeight.bold,fontStyle: FontStyle.italic)),
-                ),
             Expanded(
               child: ListView.builder(
                   itemCount: postsList.length,
                   itemBuilder: (BuildContext context, int index) {
                     return GestureDetector(
-                      onTap: (){
+                      onTap: () {
                         Toast.show('${postsList[index].title}', context,
                             duration: Toast.LENGTH_LONG, gravity: Toast.BOTTOM);
                       },
-                      child: Container(
-                        padding: const EdgeInsets.all(8),
-                        margin: EdgeInsets.all(1),
-                        height: 50,
-                        color: Colors.orange,
-                        child: Center(child: Text('${postsList[index].title}')),
+                      child: ListTile(
+                        leading: Icon(Icons.verified_user),
+                        title: Text('${postsList[index].title}'),
+                        subtitle: Text('${postsList[index].body}'),
                       ),
                     );
-                  }
-              ),
+                  }),
             )
           ]),
         ),
